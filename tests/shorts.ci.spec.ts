4be4d577ac9be491059fe6275a9668b05eb79e3f
ali adham <thehillofsunflowers@gmail.com>
2024-08-23 14:02:48 -0700

    update: Add fixture to shorts.ci tests

--- tests/shorts.ci.spec.ts
+++ tests/shorts.ci.spec.ts
@@ -1,87 +1,115 @@
-import { test, expect } from '@playwright/test';
+import { test as base, expect } from '@playwright/test';
 import { ShortsPage } from './pages/shorts-page';
 
-test('like button opens a sign in prompt', async({ page }) => {
-    const shortsPage = new ShortsPage(page);
-    await shortsPage.goto();
-    await page.setViewportSize({ width: 1960, height: 1080 });
+// ShortsPage fixture that navigates to the Shorts page
+const test = base.extend<{ shortsPage: ShortsPage }>({
+    shortsPage: async ({ page }, use) => {
+        const shortsPage = new ShortsPage(page);
+        await shortsPage.goto();
+        await use(shortsPage);
+    }
+});
 
+test('Clicking like button should prompt sign in', async({ shortsPage }) => {
+    await shortsPage.page.setViewportSize({ width: 1960, height: 1080 });
+
+    // Click like button
     await shortsPage.likeButton.click();
-    await expect(page.locator('tp-yt-iron-dropdown').getByText('Like this video?')).toBeVisible();
+
+    // Assert sign in prompt
+    await expect(shortsPage.page.locator('tp-yt-iron-dropdown').getByText('Like this video?')).toBeVisible();
 });
 
-test('dislike button opens a sign in prompt', async({ page }) => {
-    const shortsPage = new ShortsPage(page);
-    await shortsPage.goto();
-    await page.setViewportSize({ width: 1960, height: 1080 });
+test('Clicking dislike button should prompt sign in', async({ shortsPage }) => {
+    await shortsPage.page.setViewportSize({ width: 1960, height: 1080 });
 
+    // Click dislike button
     await shortsPage.dislikeButton.click();
-    await expect(page.locator('tp-yt-iron-dropdown').getByText('Don\'t like this video?')).toBeVisible();
+
+    // Assert sign in prompt
+    await expect(shortsPage.page.locator('tp-yt-iron-dropdown').getByText('Don\'t like this video?')).toBeVisible();
 });
 
-test('comments button opens the comment section with an X button', async({ page }) => {
-    const shortsPage = new ShortsPage(page);
-    await shortsPage.goto();
-    await page.setViewportSize({ width: 1960, height: 1080 });
+test('Clicking comments button should open the comment section with an X button', async({ shortsPage }) => {
+    await shortsPage.page.setViewportSize({ width: 1960, height: 1080 });
 
+    // Click comments button
     if (await shortsPage.commentsButton.isVisible()) {
         await shortsPage.commentsButton.click();
-        await expect(page.locator(`[id="\\3${shortsPage.shortsIterator}"]`).getByTitle('Comments')).toBeVisible();
-        await page.getByRole('button', { name: 'Close' }).click();
-    } else if(await page.locator(`[id="\\3${shortsPage.shortsIterator}"]`).getByLabel('Disabled', { exact: true }).isVisible()) {
-        await page.locator(`[id="\\3${shortsPage.shortsIterator}"]`).getByLabel('Disabled', { exact: true }).click();
-    }
+
+        // Assert comments section is visible
+        await expect(shortsPage.page.locator(`[id="\\3${shortsPage.shortsIterator}"]`).getByTitle('Comments')).toBeVisible();
+
+        // Close comments section
+        await shortsPage.page.getByRole('button', { name: 'Close' }).click();
+
+        // If improperly loaded, press Disabled locator instead to assert template existence
+    } /*else if(await shortsPage.page.locator(`[id="\\3${shortsPage.shortsIterator}"]`).getByLabel('Disabled', { exact: true }).isVisible()) {
+        await shortsPage.page.locator(`[id="\\3${shortsPage.shortsIterator}"]`).getByLabel('Disabled', { exact: true }).click();
+    }*/
 });
 
-test('share button opens a popup with 12 social media platforms', async({ page }) => {
-    const shortsPage = new ShortsPage(page);
-    await shortsPage.goto();
-    await page.setViewportSize({ width: 1960, height: 1080 });
+test('Clicking share button opens a popup with 12 social media platforms', async({ shortsPage }) => {
+    await shortsPage.page.setViewportSize({ width: 1960, height: 1080 });
 
+    // Click share button
     await shortsPage.clickShareButton();
-    await expect(page.getByRole('listbox')).toBeVisible();
-    const socialsList = await page.getByLabel('Select an application to share with').locator('yt-share-target-renderer').all();
+
+    // Assert popup menu is visible
+    await expect(shortsPage.page.getByRole('listbox')).toBeVisible();
+
+    // Assert 12 social media links are visible
+    const socialsList = await shortsPage.page.getByLabel('Select an application to share with').locator('yt-share-target-renderer').all();
     expect(socialsList).toHaveLength(12);
-    await page.getByLabel('Cancel').click();
+
+    // Close menu
+    await shortsPage.page.getByLabel('Cancel').click();
 });
 
-test('more actions button opens a menu', async({ page }) => {
-    const shortsPage = new ShortsPage(page);
-    await shortsPage.goto();
-    await page.setViewportSize({ width: 1960, height: 1080 });
+test('Clicking more actions button opens a menu', async({ shortsPage }) => {
+    await shortsPage.page.setViewportSize({ width: 1960, height: 1080 });
 
+    // Click more actions button
     await shortsPage.moreActionsButton.click();
-    await expect(page.getByText('Description Captions Report Send Feedback')).toBeVisible();
+
+    // Assert that options menu shows up
+    await expect(shortsPage.page.getByText('Description Captions Report Send Feedback')).toBeVisible();
 });
 
-test('Next and Previous Buttons Navigate Correctly', async({ page }) => {
-    const shortsPage = new ShortsPage(page);
-    await shortsPage.goto();
-    
-    if(await page.locator(`[id="\\3${shortsPage.shortsIterator}"]`).getByLabel('Play', { exact: true }).isVisible()) {
-        await page.locator(`[id="\\3${shortsPage.shortsIterator}"]`).getByLabel('Play', { exact: true }).click();
+test('Next and Previous Buttons Navigate Correctly', async({ shortsPage }) => {
+    // Function to click play if visible
+    async function clickPlay(): Promise<void> {
+        if (await shortsPage.page.locator(`[id="\\3${shortsPage.shortsIterator}"]`).getByLabel('Play', { exact: true }).isVisible()) {
+            await shortsPage.page.locator(`[id="\\3${shortsPage.shortsIterator}"]`).getByLabel('Play', { exact: true }).click();
+        }
     }
 
+    await clickPlay();
+
+    // Get first shorts thumbnail
     const firstThumbnail = await (await shortsPage.getShortsThumbnail()).getAttribute('style');
+
+    // Press next short button
     await shortsPage.navigateToNextShort();
 
+    // Assert thumbnail existence
     await expect(await shortsPage.getShortsThumbnail()).toHaveAttribute('style', /.+frame0\.jpg/);
+
+    // Get second shorts thumbnail
     const secondThumbnail = await (await shortsPage.getShortsThumbnail()).getAttribute('style');
 
-    if(await page.locator(`[id="\\3${shortsPage.shortsIterator}"]`).getByLabel('Play', { exact: true }).isVisible()) {
-        await page.locator(`[id="\\3${shortsPage.shortsIterator}"]`).getByLabel('Play', { exact: true }).click();
-    }
+    await clickPlay();
 
+    // Assert that the thumbnail has changed
     expect(secondThumbnail).not.toBe(firstThumbnail);
 
-    if(await page.locator(`[id="\\3${shortsPage.shortsIterator}"]`).getByLabel('Play', { exact: true }).isVisible()) {
-        await page.locator(`[id="\\3${shortsPage.shortsIterator}"]`).getByLabel('Play', { exact: true }).click();
-    }
+    await clickPlay();
 
     // Check previous short in the shorts inner container
-    await expect(page.locator(`[id="\\3${shortsPage.shortsIterator--}"]`).locator('.player-container')).toHaveAttribute('style', /.+frame0\.jpg/);
-    const currentThumbnail = await (page.locator(`[id="\\3${shortsPage.shortsIterator--}"]`).locator('.player-container')).getAttribute('style');
+    await expect(shortsPage.page.locator(`[id="\\3${shortsPage.shortsIterator--}"]`).locator('.player-container')).toHaveAttribute('style', /.+frame0\.jpg/);
+    const currentThumbnail = await (shortsPage.page.locator(`[id="\\3${shortsPage.shortsIterator--}"]`).locator('.player-container')).getAttribute('style');
     
+    // Assert that the previous short is the first short and not the second
     expect(currentThumbnail).toBe(firstThumbnail);
     expect(currentThumbnail).not.toBe(secondThumbnail);
 });
