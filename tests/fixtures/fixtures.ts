e2c937d8542ceaf9c63e67b6354e418da3bd021a
ali adham <thehillofsunflowers@gmail.com>
2024-09-06 13:04:17 -0700

    feat: Add generic createFixture function in fixtures file

--- tests/fixtures/fixtures.ts
+++ tests/fixtures/fixtures.ts
@@ -1,8 +1,17 @@
-import { test as base } from '@playwright/test';
+import { test as base, Page } from '@playwright/test';
 import { HomePage } from '../pages/home-page';
 import { ShortsPage } from '../pages/shorts-page';
 import { VideoPage } from '../pages/video-page';
 import { SearchResultsPage } from '../pages/search-results-page';
+
+// Generic function that creates our page objects within our extended test
+const createFixture = <T>(PageObject: { new (page: Page): T }) => async (
+    { page }: { page: Page },
+    use: (Fixture: T) => Promise<void>
+) => {
+    const pageObject = new PageObject(page);
+    await use(pageObject);
+};
 
 // Define Typing for Fixtures
 type MyFixtures = {
@@ -14,25 +23,8 @@ type MyFixtures = {
 
 // Extend test to contain all our fixtures, which can be deconstructed in the respective test
 export const test = base.extend<MyFixtures>({
-    homePage: async ({ page }, use) => {
-        const homePage = new HomePage(page);
-        await use(homePage);
-    },
-    
-    shortsPage: async ({ page }, use) => {
-        const shortsPage = new ShortsPage(page);
-        await use(shortsPage);
-    },
-
-    videoPage: async({ page }, use) => {
-        const videoPage = new VideoPage(page);
-        await use(videoPage);
-    },
-
-    searchResultsPage: async ({ page }, use) => {
-        const searchResultsPage = new SearchResultsPage(page);
-        await use(searchResultsPage);
-    },
+    homePage: createFixture(HomePage),
+    shortsPage: createFixture(ShortsPage),
+    videoPage: createFixture(VideoPage),
+    searchResultsPage: createFixture(SearchResultsPage),
 });
-
-export { expect } from '@playwright/test';
