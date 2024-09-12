690cb7d50fe8426a46574bb2231b72d3448d54bd
ali adham <thehillofsunflowers@gmail.com>
2024-09-12 00:03:20 -0700

    Revert CI Reporting changes

--- playwright.ci.config.ts
+++ playwright.ci.config.ts
@@ -23,7 +23,7 @@ module.exports = defineConfig({
   workers: process.env.CI ? 4 : undefined,
   grep: testPlanFilter(),
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? 'allure-playwright' : 'blob',
+  reporter: process.env.CI ? 'blob' : 'allure-playwright',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
