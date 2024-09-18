f5bb0364033c673adec7dc4b8c7a9135acb685b9
ali adham <thehillofsunflowers@gmail.com>
2024-09-17 22:51:08 -0700

    Test GH Actions with 1 worker, 4 shards

--- playwright.ci.config.ts
+++ playwright.ci.config.ts
@@ -20,7 +20,7 @@ module.exports = defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 4 : undefined,
+  workers: process.env.CI ? 1 : undefined,
   grep: testPlanFilter(),
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: process.env.CI ? 'blob' : 'allure-playwright',
