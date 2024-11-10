5b6f676b8486c6813a5a5a31b38a2571bab9da19
ali adham <thehillofsunflowers@gmail.com>
2024-11-09 20:58:23 -0800

    update: npm and add eslint for Playwright

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+import playwright from 'eslint-plugin-playwright'
+
+export default [
+  {
+    ...playwright.configs['flat/recommended'],
+    files: ['tests/**'],
+    rules: {
+      ...playwright.configs['flat/recommended'].rules,
+      // Customize Playwright rules
+      // ...
+    },
+  },
+]
